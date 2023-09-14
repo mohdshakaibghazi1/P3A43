90601beb1852d9bc677f94592633fee87183d21b
Mohd Shakaib Ghazi <mohd.shakaibghazi@mphasis.com>
2023-09-14 15:50:56 +0530

    first

---
+++ TaskWebApi/TaskWebApi/Data/TaskWebApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskWebApi.Models;
+
+namespace TaskWebApi.Data
+{
+    public class TaskWebApiContext : DbContext
+    {
+        public TaskWebApiContext (DbContextOptions<TaskWebApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaskWebApi.Models.Task> Task { get; set; } = default!;
+    }
+}
---
+++ TaskWebApi/TaskWebApi/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskWebApi.Models
+{
+  
+    [Table("Task")]
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
+
+
